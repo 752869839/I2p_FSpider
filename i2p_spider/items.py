8cb0839c0aea67801477b4c56726f09b797c05dc
陈立志 <44913268+752869839@users.noreply.github.com>
2021-03-09 10:39:56 +0800

    Update items.py

--- i2p_spider/items.py
+++ i2p_spider/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class I2pWholeNetworkItem(scrapy.Item):
+class HtmlItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     url = scrapy.Field()
