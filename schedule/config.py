a7829ebd840cde45eef23f8134b95131e736105f
752869839 <chen752869839@qq.com>
2021-04-08 14:51:06 +0800

    1

---
+++ schedule/config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from redis import StrictRedis
+from elasticsearch import Elasticsearch
+
+#redis
+r_host='172.16.30.45'
+r_port=6379
+db=0
+
+# elasticsearch
+e_host='172.16.30.68'
+e_port=9200
+
+redis_client = StrictRedis(host=r_host, port=r_port, db=db)
+
+es_client = Elasticsearch(hosts=e_host, port=e_port, timeout=30, max_retries=10)
